183bf5617a9a3ddb7075aa12ca79cc20779b7fa9
anetabarone <anetabarone@yahoo.com>
2023-05-13 11:34:59 +0200

    added link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <br />
+        <small>
+          <a href="https://github.com/anetabarone/weather-react" target="blank">
+            Open-sourced code
+          </a>{" "}
+          by Aneta Barone, 2023
+        </small>
       </header>
     </div>
   );
